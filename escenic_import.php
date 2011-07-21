ddd5130c0fd0f0ff1290631def69e94a5ef44442
Christian Bolstad <christian@bolstad.se>
2011-07-21 22:53:13 +0000

    fetching the_title + post_excerpt + post_content

--- escenic_import.php
+++ escenic_import.php
@@ -7,6 +7,10 @@
         if (is_object($data)) $data = get_object_vars($data);
         return is_array($data) ? array_map(__FUNCTION__, $data) : $data;
     }
+
+    function replace_newline($string) {
+      return (string)str_replace(array("\r", "\r\n", "\n"), '', $string);
+    }
     
   if (file_exists($escenic_source)) 
     {
@@ -17,20 +21,63 @@
                 $type = $content_piece->attributes()->type;
                 if ($type == 'article')
                         {
+#                                        print_r($content_piece);
+ #               die;
+
                             print $content_piece->uri . "\n";                   
                             print "state : " .  $content_piece->attributes()->state . "\n";
                             echo "Yay, found an article\n";
+                            
                             foreach ($content_piece->field as $article_field)
                                 {
+                                    # the_content
+                                    if ($article_field->attributes()->name == 'headline')
+                                    {
+                                        $postobj['the_title'] = replace_newline($article_field[0]);
+                                    }
+
                                     if ($article_field->attributes()->name == 'body')
                                         {
+                                            $thetext = '';
                                             foreach ($article_field->p as $line)
                                                 {
-                                                    # the original web article + xml got paragraphs, so we are adding them aswell                                                   
-                                                    $thetext .= "<p>$line</p>\n";
+                                                    if (gettype($line) == 'object')
+                                                        {
+#                                                            var_dump(get_object_vars($line));
+                                                            # the original web article + xml got paragraphs, so we are adding them aswell                                                   
+                                                            $thetext .= "<p>$line</p>\n";
+                                                        }
+                                                        else
+                                                        {
+#                                                            print_r($line);
+                                                            echo gettype($line);
+                                                        }
                                                 }
                                            $postobj['post_content'] = $thetext ;
+                                        }
+                                    # post_excerpt
+                                    if ($article_field->attributes()->name == 'leadtext')
+                                        {
 
+                                            print_r($article_field);
+                                            $post_excerpt = '';
+                                            foreach ($article_field->p as $line)
+                                                {
+                                                
+                                                    if ($keys = array_keys(get_object_vars($line)))
+                                                        {
+                                                            print_r($keys);
+                                                            foreach ($keys as $key)
+                                                                {
+                                                                    $post_excerpt .= "<$key>". $line->$key. "</$key>\n";
+                                                                    
+                                                                }
+                                                        }
+
+                                                    # the original web article + xml got paragraphs, so we are adding them aswell                                                   
+                                                    $post_excerpt .= "<p>$line</p>\n";
+                                                }
+                                           $postobj['post_excerpt'] = $post_excerpt ;
                                         }
                                                                 
                                 }
